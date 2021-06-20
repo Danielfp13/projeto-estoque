cedb3793d43e146abe484c90d986f705d786b5a1
Daniel <daniel.si@outlook.com.br>
2021-06-20 12:18:09 -0300

    ciclos de vida dos componentes

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 
-class ClassComponent extends React.Component< {name: string}>{
-   state = {
-      name:"Daniel"
+class ClassComponent extends React.Component<{ name: string }> {
+   constructor(props: any){
+      super(props)
+      console.log('contrutor reached')
    }
-   render(){
-      return  <div>
-         <p>name:{ this.state.name }</p>
-         <button onClick = { () =>{
-            this.setState( {name: "Natalia "} )
-         }}>Click aqui!</button>
-         <p>Olá {this.state.name} {this.props.name}, esse é um componente baseado em classe!!!</p>
-         </div>
-   }
+  state = {
+    name: "Daniel",
+  };
+
+  componentDidMount(){
+     console.log('did mount reached')
+  }
+
+  componentDidUpdate(){
+   console.log('did update reached')
+  }
+  
+  render() {
+     console.log('render reached')
+    return (
+      <div>
+        <p>name:{this.state.name}</p>
+        <button
+          onClick={() => {
+            this.setState({ name: "Natalia " });
+          }}>
+          Click aqui!
+        </button>
+        <p>
+          Olá {this.state.name} {this.props.name}, esse é um componente baseado
+          em classe!!!
+        </p>
+      </div>
+    );
+  }
 }
-export default ClassComponent
+export default ClassComponent;
