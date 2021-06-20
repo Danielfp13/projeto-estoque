cb086c449d6d367ce87b9e2890703f5956efa41a
Daniel <daniel.si@outlook.com.br>
2021-06-20 12:51:45 -0300

    uso de state

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <TestComponent />
+       <TestComponent propiedade="Juliana" />
        <ClassComponent  name="Pereira"/>
         <a
           className="App-link"
--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TestComponent.css'
-const TestComponent = () =>(
+/* const TestComponent = () =>(
    <div className="TestComponent">Test Component </div>
-)
-
+) */
+function TestComponent (props: { propiedade: string} ) {
+const [age,setAge] = useState(32)
+   return    <div className="TestComponent">
+      Test Component Olá { props.propiedade } , sua idade é, { age }
+      <button onClick = { () => {
+         setAge(age +1)
+      }}> + </button>
+       </div>
+}
 export default TestComponent
