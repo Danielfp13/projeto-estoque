937c9f59207b3b477e9d0ba51a79b6d7e4367d86
Daniel <daniel.si@outlook.com.br>
2021-06-22 20:50:30 -0300

    preparando para criar tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,20 @@
-import React, { useState } from "react";
-import Button from "../../shared/Button";
+import React from "react";
+
 import Container from "../../shared/Container";
 import Header from "../Header";
-import Input from "../../shared/Input";
 
 import "./App.css";
-import useEffect from 'react';
 
-function TestComponent() {
-  return (
-    <img
-      width="16px"
-      src="https://i.pinimg.com/originals/5e/26/79/5e2679281fffe61007e13028b24602a1.png"
-      alt="icone"
-    />
-  );
-}
+
 
 function App() {
-
-  const[street, setStreet] = useState('')
 
   return (
     <div className="App">
       <Header title="Stock" />
 
       <Container>
-        <ul>
-        {
-          ['Laranja', 'Maça', 'Morango', 'Uva', 'Pera'] .map( (fruta, index) =>{
-            return <li key={ index }>
-              { fruta }
-            </li>
-          })
-        }
-    </ul>
+ 
       </Container>
     </div>
   );
